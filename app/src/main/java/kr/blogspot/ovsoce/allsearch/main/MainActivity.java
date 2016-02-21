93d0862dbc0899143fdffcbe7704b7e02e62c124
jaeho_oh <ovsoc@daum.net>
2016-02-21 15:42:40 +0900

    1. 탭 버튼으로 탭이동 구현.

--- app/src/main/java/kr/blogspot/ovsoce/allsearch/main/MainActivity.java
+++ app/src/main/java/kr/blogspot/ovsoce/allsearch/main/MainActivity.java
@@ -159,16 +159,16 @@ public class MainActivity extends AppCompatActivity implements MainPresenter.Vie
         tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-
+                mViewPager.setCurrentItem(tab.getPosition(), true);
             }
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
-
             }
 
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
+                Log.d("");
                 PlaceholderFragment fragment = (PlaceholderFragment) mSectionsPagerAdapter.getRegisteredFragment(tab.getPosition());
                 fragment.getPresenter().onQueryTextSubmit(getApplicationContext(), fragment.getArguments());
             }
